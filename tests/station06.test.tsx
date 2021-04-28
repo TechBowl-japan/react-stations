3c306f9dff5f56302821c3574bf86304c7689006
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2021-04-28 16:12:09 +0900

    Use fetch mock

--- tests/station06.test.tsx
+++ tests/station06.test.tsx
@@ -2,13 +2,58 @@ import React from 'react'
 import renderer, { act } from 'react-test-renderer'
 import { App } from '../src/App'
 
+const imageUrl = 'https://avatars.githubusercontent.com/u/298748'
+const mockServerResponse = {
+  message: imageUrl,
+  status: 'success',
+}
+const mockResponse: Response = {
+  headers: {} as any,
+  ok: true,
+  redirected: false,
+  status: 200,
+  statusText: 'OK',
+  trailer: {} as any,
+  type: 'default',
+  body: null,
+  url: '',
+  bodyUsed: false,
+  arrayBuffer() {
+    throw new Error('unimplemented; You are doing something wrong.')
+  },
+  blob() {
+    throw new Error('unimplemented; You are doing something wrong.')
+  },
+  formData() {
+    throw new Error('unimplemented; You are doing something wrong.')
+  },
+  json() {
+    return Promise.resolve(JSON.parse(JSON.stringify(mockServerResponse)))
+  },
+  text() {
+    return Promise.resolve(JSON.stringify(mockServerResponse))
+  },
+  clone() {
+    throw new Error('unimplemented; You are doing something wrong.')
+  },
+}
+
 describe('<App />', () => {
   const callback = {
     run: (_: string) => {},
   }
 
-  const fetchSpy = jest.spyOn(window, 'fetch')
+  const fetch = jest.fn()
   const useStateSpy = jest.spyOn(React, 'useState')
+
+  window.fetch = fetch
+
+  fetch.mockImplementation((resource, ..._) => {
+    const url = typeof resource === 'string' ? resource : resource.url
+    return Promise.resolve({
+      ...mockResponse, url,
+    })
+  })
 
   useStateSpy.mockImplementation((value?: unknown) => {
     return [
@@ -19,7 +64,7 @@ describe('<App />', () => {
     ] as any
   })
 
-  it('setState is called with a random value', async done => {
+  it('fetch() is called and the response value is used', async done => {
     const res = renderer.create(<App />)
     const img = res.root.findByType('img')
     const button = res.root.findByType('button')
@@ -29,8 +74,8 @@ describe('<App />', () => {
 
     callback.run = (value: string) => {
       try {
-        expect(fetchSpy).toBeCalled()
-        expect(value).not.toStrictEqual(initialImg)
+        expect(fetch).toBeCalled()
+        expect(value).toStrictEqual(imageUrl)
         done()
       } catch (e) {
         done.fail(e)
