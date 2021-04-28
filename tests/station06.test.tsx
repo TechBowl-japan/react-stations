3fe04d4f3401ea051db3db1cc83565a69097ead8
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2021-04-28 16:40:59 +0900

    Add test for Station No. 8

--- tests/station06.test.tsx
+++ tests/station06.test.tsx
@@ -1,42 +1,7 @@
 import React from 'react'
 import renderer, { act } from 'react-test-renderer'
 import { App } from '../src/App'
-
-const imageUrl = 'https://avatars.githubusercontent.com/u/298748'
-const mockServerResponse = {
-  message: imageUrl,
-  status: 'success',
-}
-const mockResponse: Response = {
-  headers: {} as any,
-  ok: true,
-  redirected: false,
-  status: 200,
-  statusText: 'OK',
-  trailer: {} as any,
-  type: 'default',
-  body: null,
-  url: '',
-  bodyUsed: false,
-  arrayBuffer() {
-    throw new Error('unimplemented; You are doing something wrong.')
-  },
-  blob() {
-    throw new Error('unimplemented; You are doing something wrong.')
-  },
-  formData() {
-    throw new Error('unimplemented; You are doing something wrong.')
-  },
-  json() {
-    return Promise.resolve(JSON.parse(JSON.stringify(mockServerResponse)))
-  },
-  text() {
-    return Promise.resolve(JSON.stringify(mockServerResponse))
-  },
-  clone() {
-    throw new Error('unimplemented; You are doing something wrong.')
-  },
-}
+import { mockResponse, imageUrl } from './mock/fetch'
 
 describe('<App />', () => {
   const callback = {
---
+++ tests/station08.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import renderer, { act } from 'react-test-renderer'
+import { mockResponse, imageUrl } from './mock/fetch'
+
+describe('<App />', () => {
+  const fetch = jest.fn()
+
+  window.fetch = fetch
+
+  fetch.mockImplementation((resource, ..._) => {
+    const url = typeof resource === 'string' ? resource : resource.url
+    return Promise.resolve({
+      ...mockResponse,
+      url,
+    })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('Can mount <App />', () => {
+    const { App } = require('../src/App')
+    expect(App).toBeTruthy()
+    renderer.create(<App />)
+  })
+
+  it('Can mount <Header />', () => {
+    const { Header } = require('../src/Header')
+    expect(Header).toBeTruthy()
+    renderer.create(<Header />)
+  })
+
+  it('Can mount <Description />', () => {
+    const { Description } = require('../src/Description')
+    expect(Description).toBeTruthy()
+    renderer.create(<Description />)
+  })
+
+  it('Can mount <RandomDogButton />', () => {
+    const { RandomDogButton } = require('../src/RandomDogButton')
+    expect(RandomDogButton).toBeTruthy()
+    renderer.create(<RandomDogButton />)
+  })
+
+  it('<RandomDogButton /> has a prop called `handleClickRandomButton` which is called with a image from API', async done => {
+    const { RandomDogButton } = require('../src/RandomDogButton')
+    expect(RandomDogButton).toBeTruthy()
+
+    const mockHandler = jest.fn()
+    mockHandler.mockImplementation((image: string) => {
+      expect(image).toStrictEqual(imageUrl)
+      done()
+    })
+    const res = renderer.create(
+      <RandomDogButton handleClickRandomButton={mockHandler} />,
+    )
+
+    const button = res.root.findByType('button')
+    act(() => {
+      button.props.onClick()
+    })
+  })
+})
