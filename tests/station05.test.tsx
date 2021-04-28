4bdcd26bab2cf9b686d7565f3a60081ab7b70f35
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2021-04-28 15:32:14 +0900

    Implement test for No. 5

---
+++ tests/station05.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import renderer, { act } from 'react-test-renderer'
+import { App } from '../src/App'
+
+describe('<App />', () => {
+  let setState: React.Dispatch<unknown> | undefined = undefined
+  const useState = React.useState
+  const useStateSpy = jest.spyOn(React, 'useState')
+  useStateSpy.mockImplementation((v?: unknown) => {
+    const [value, dispatcher] = useState(v)
+    setState = dispatcher
+    return [value, dispatcher]
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('<App /> calls useState', () => {
+    renderer.create(<App />)
+
+    expect(useStateSpy).toBeCalled()
+  })
+
+  it('state changes when the button is clicked', () => {
+    const res = renderer.create(<App />)
+    const img = res.root.findByType('img')
+    const button = res.root.findByType('button')
+
+    const initialImg = img.props.src
+    expect(initialImg).not.toBeFalsy()
+
+    act(() => {
+      button.props.onClick()
+    })
+
+    const changedImg = img.props.src
+    expect(changedImg).not.toBeFalsy()
+
+    expect(changedImg).not.toStrictEqual(initialImg)
+  })
+})
