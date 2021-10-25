ff81a7a0dd18d0ec96fdc61414f5d05ff168effa
niwa-s <souta.niwa.prog@gmail.com>
2021-10-25 22:49:33 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ export const App = () => {
   return (
     <div>
       <header>Dobアプリ</header>
+      <p>このサイトは犬の画像を表示するサイトです</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="可愛い犬の画像" />
     </div>
   )
 }
