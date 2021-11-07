4dc0380665a0980dee9b95dac6c56f150d4a8662
kenchikit <inaken1127@gmail.com>
2021-11-08 01:41:44 +0900

    useStateで画像を表示

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@
 
 import * as React from 'react'
 import './App.css'
+import { useState } from 'react';
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+  const [url, setDogUrl] = useState("https://images.dog.ceo/breeds/eskimo/n02109961_21096.jpg");
   return (
     <div>
       <header>
         <h1>Dogアプリ</h1>
       </header>
       <p>犬の画像を表示するサイトです</p>
-      <img src={"https://images.dog.ceo/breeds/eskimo/n02109961_21096.jpg"} />;
+      <img src={ url } />;
     </div>
   )
 }
