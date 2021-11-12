900cfe27733cfbf9274f83ae02366f07cbff0bb3
kenchikit <inaken1127@gmail.com>
2021-11-12 22:02:36 +0900

    appjsから読み込み

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,8 @@ import * as React from 'react'
 
 export function Header(props) {
   return (
-    <div className="App-header">
-      <header>
-        <h1>{props.title}</h1>
-      </header>
-    </div>
+    <header>
+      <h1>{props.title}</h1>
+    </header>
   )
 }
