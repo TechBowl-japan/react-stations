a3f03bdfb39758e5ded3b81ea736afa98326f0d5
kenchikit <inaken1127@gmail.com>
2021-11-12 22:29:27 +0900

    appから読み込む

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import * as React from 'react'
 import './App.css'
 import { Header } from './Header.js'
 import { Description } from './Description.js';
-
+import { Dogimage } from './DogImage.js';
+import { useState } from 'react';
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogurl, setDogUrl] = useState("https://images.dog.ceo/breeds/eskimo/n02109961_21096.jpg");
+  const handleClick = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => setDogUrl(data.message));
+  }
+
   return (
     <div>
       <Header title="Dogアプリ" />
-      <Description
-        desc="犬の画像を表示するサイトです"
-        text="更新"  
-      />
+      <div className="wrap">
+        <Description desc="犬の画像を表示するサイトです" />
+        <span className="item">
+          <Dogimage url={dogurl} />
+          <button className="btn" onClick={handleClick}>
+            更新
+          </button>
+        </span>
+      </div>
     </div>
   )
 }
--- src/Description.js
+++ src/Description.js
@@ -1,27 +1,10 @@
 // DO NOT DELETE
 import * as React from 'react'
-import { Dogimage } from './DogImage.js';
-import { useState } from 'react';
 
 export function Description(props){
-  const [dogurl, setDogUrl] = useState("https://images.dog.ceo/breeds/eskimo/n02109961_21096.jpg");
-  const handleClick = () => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then(response => response.json())
-      .then(data => setDogUrl(data.message));
-  }
-
   return(
-    <div className="wrap">
-      <span className="item">
-        {props.desc}
-      </span>
-      <span className="item">
-        <Dogimage url={dogurl} />
-        <button className="btn" onClick={handleClick}>
-          {props.text}
-        </button>
-      </span>
-    </div>
+    <span className="item">
+      {props.desc}
+    </span>
   )
 }
