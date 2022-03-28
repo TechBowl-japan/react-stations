861b92294fc885325132cd8607b88c1a6509c598
SuguruOoki <oki.work0527@gmail.com>
2022-03-28 11:31:24 +0900

    [MODIFY]Meeting->Unittestのための下準備

--- tests/station07.test.tsx
+++ tests/station07.test.tsx
@@ -1,4 +1,6 @@
 // No tests for Station No.7.
 // Proceed to the meeting reservation.
+// TODO: ここのテストを書く
+// TODO: クリアしてほしい基準を決定→たたきを大木へ送る(→問題文からみて決定する)
 
 test.skip('No tests for Station No.7', () => {})
