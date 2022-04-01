e9ff81b9e02065ed7786874be289527293cad374
Àżu <53806909+Azu0925@users.noreply.github.com>
2022-04-01 12:39:33 +0900

    Revert "問題の修正"

--- tests/station07.test.tsx
+++ tests/station07.test.tsx
@@ -1,6 +1,4 @@
 // No tests for Station No.7.
 // Proceed to the meeting reservation.
-// TODO: ここのテストを書く
-// TODO: クリアしてほしい基準を決定→たたきを大木へ送る(→問題文からみて決定する)
 
 test.skip('No tests for Station No.7', () => {})
