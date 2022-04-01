a5a91e1abccb94caf85581c531cf9ae3058bc3a5
Azu0925 <yamaneko564783@gmail.com>
2022-04-01 15:43:28 +0900

    station7 cypress->jest

--- tests/station07.test.tsx
+++ tests/station07.test.tsx
@@ -1,6 +1,24 @@
-// No tests for Station No.7.
-// Proceed to the meeting reservation.
-// TODO: ここのテストを書く
-// TODO: クリアしてほしい基準を決定→たたきを大木へ送る(→問題文からみて決定する)
+import { render } from '@testing-library/react'
+import React from 'react'
+import { createAsync } from './utils/createAsync'
+import { compareColor } from './utils/compareColor'
 
-test.skip('No tests for Station No.7', () => {})
+const { App } = require('../src/App') as { App: React.ComponentType<{}> }
+
+describe('<App />', () => {
+  it('<App /> has a <header>', async () => {
+    const res = await createAsync(<App />)
+    res.root.findByType('header')
+  })
+
+  it('#F5F5F5 is specified as the background color of <header>', async () => {
+    const { container } = render(<App />)
+    const header = container.querySelector('header')
+
+    if(!header){
+      return
+    }
+
+    expect(compareColor(window.getComputedStyle(header).backgroundColor, '#f5f5f5')).toBe(true)
+  })
+})
