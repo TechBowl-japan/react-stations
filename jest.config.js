7d155b6a42a9694217aba49f9d2a22ca307715e4
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2022-04-30 16:57:15 +0900

    deps: update dependencies; support Jest 28

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'js', 'tsx'],
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-    "^.+\\.js$": "babel-jest",
-    "^.+\\.css$": "jest-transform-css"
+    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.css$': 'jest-transform-css',
   },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
     },
   },
-  moduleNameMapper: {
-  },
+  moduleNameMapper: {},
 }
