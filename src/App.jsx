6dd10b33f41e95a970c2265d516af2aee8965aad
Yuto0508 <yuto4139@outlook.jp>
2023-09-29 15:37:06 +0900

    main

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ export const App = () => {
       <header>
         <h1>To Doアプリ</h1>
       </header>
+      <p>犬の画像を表示するサイトです。</p>
+      <br></br>
+      <img src="https://images.dog.ceo/breeds/pomeranian/n02112018_5540.jpg"
+      alt="可愛い犬"
+      />
+      {/* 犬の画像と画像の説明文を表示させる。。 */}
     </div>
   )
 }
