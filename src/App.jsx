5c2f4e5dab682af45a2db4d841d068b3f8fcf99a
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-02-07 11:14:05 +0900

    サイトの説明と画像の追加

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ import './App.css'
 export const App = () => {
   return (
     <div>
-      <header>初めてのReact</header>
+      <header>
+        <h1>初めてのReact</h1>
+      </header>
+      <p>犬の画像を表示するサイトです！</p>
+      <img
+        src="https://images.dog.ceo/breeds/stbernard/n02109525_10908.jpg"
+        alt="サイトから持ってきた犬の画像"
+      />
     </div>
   )
 }
