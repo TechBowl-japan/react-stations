ccdf152d8802a8270500d3f7b375e54f026356a3
nihei-shunsuke <c1041462@planet.kanazawa-it.ac.jp>
2024-11-26 21:59:32 +0900

    station4 clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // DO NOT DELETE
 
 import './App.css'
-
+import { useState } from 'react'
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://dog.ceo/api/breeds/image/random')
   return (
     <div>
       <header>React Station 初級</header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src='https://dog.ceo/api/breeds/image/random' />
+      <img src= {dogUrl}/>
     </div>
   )
 }
