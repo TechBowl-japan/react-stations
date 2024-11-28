85a472184f8db274c050af49d032e13dfe7b8a50
[roddyPiper54] <[hiroyuki.nakajima1887@gmail.com]>
2024-11-28 23:04:15 +0900

    #3 done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ export const App = () => {
   return (
     <div>
       <header>アプリ</header>
+      <img
+        src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"
+        alt="dog"
+      />
     </div>
   )
 }
