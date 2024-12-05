c8b462ff1b7066efd5dbc5d053f038a0268776bc
[roddyPiper54] <[hiroyuki.nakajima1887@gmail.com]>
2024-12-06 01:23:01 +0900

    #6 done

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,44 @@ export const App = () => {
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
   )
 
-  // setDogUrl(dogUrl)
+  const onClickCUrl = () => {
+    //urlを定義
+    //const randomUrl;
+    //fetchでapi呼び出す
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => {
+        //status
+        console.log(response.status)
+        //status 200以外
+        if (!response.ok) {
+          console.log('error')
+
+          setDogUrl(
+            'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg',
+          )
+        } else {
+          //status 200のとき
+          return response.json()
+          //return response.json(); を書くことで、次の .then にパース済みの JSON データが渡される。
+          // これにより、非同期処理のチェーンが正しく動作する。
+        }
+      })
+      .then(json => {
+        if (json) {
+          setDogUrl(json.message)
+        }
+      })
+  }
 
   return (
-    <div>
+    <>
       <header>アプリ</header>
-      <img src={dogUrl} alt="dog" />
-    </div>
+      <main>
+        <div>
+          <img src={dogUrl} alt="dog" />
+        </div>
+        <button onClick={onClickCUrl}>change URL</button>
+      </main>
+    </>
   )
 }
