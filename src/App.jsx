b4306f3b2ef6ace27283e779af395e86829b83c4
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-09 17:07:05 +0900

    station6｜リファクタ｜非同期を.thenでなくasync/awaitでかく

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,27 @@ import { useState } from 'react'
 import './App.css'
 
 /**
- * ①JSXとは？
+ *
  * @type {() => JSX.Element}
  */
 export const App = () => {
 
   const DEFAULT_DOG_URL = 'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg'
   // const NEW_DOG_URL = 'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg'
-
+  const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random'
   
 
   // useState を使って dogUrl という state を定義
-  // ② setDogurlはなぜ必要？
   const [dogurl, setDogurl] = useState(DEFAULT_DOG_URL)
 
-  const imgUpdate = () => {
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => setDogurl(data.message))
-    .catch(error => console.error('Error fetching dog image:', error))
+  const imgUpdate = async () => {
+    try {
+      const response = await fetch(DOG_API_URL)
+      const data = await response.json()
+      setDogurl(data.message)
+    } catch (error) {
+      console.error('Error fetching dog image:', error)
+    }
   }
   const imgReset = () => {
     setDogurl(DEFAULT_DOG_URL)
@@ -40,14 +42,13 @@ export const App = () => {
 
 
 // メモ
-// ①JSX（JavaScript XML）は「ジェイエスエックス」と読みます。JavaScriptの構文拡張で、ReactでUIを記述するために使用されます。
+// JSX（JavaScript XML）は「ジェイエスエックス」と読みます。JavaScriptの構文拡張で、ReactでUIを記述するために使用されます。
 // JSXは、HTMLのようなコードをJavaScriptファイル内に記述できます
 
 // useState
 // state は useState という関数（React 内では Hooks といいます）を使うことで定義できる
 // 疑問：useStateはReactのフック？それともvue3のuseStateと同じ？
 
-// ② 
 // const [dogurl, setDogurl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
 // Reactでは、状態（state）を直接変更することはできない。setDogurlは状態を更新するための関数です。
 // 疑問：なぜ直接変更できないのか？
