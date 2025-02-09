7f81dd58c23ba5cc4578533e0f77b5efbfe6f1c3
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-09 16:41:31 +0900

    station4

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 // DO NOT DELETE
-
+import { useState } from 'react'
 import './App.css'
 
 /**
+ * ①JSXとは？
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  // useState を使って dogUrl という state を定義
+  // ② setDogurlはなぜ必要？
+  const [dogurl, setDogurl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
+
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="犬の画像" />
+      <img src={dogurl} alt="犬の画像" />
     </div>
   )
 }
 
 
 // メモ
-// JSX（JavaScript XML）は「ジェイエスエックス」と読みます。JavaScriptの構文拡張で、ReactでUIを記述するために使用されます。
+// ①JSX（JavaScript XML）は「ジェイエスエックス」と読みます。JavaScriptの構文拡張で、ReactでUIを記述するために使用されます。
 // JSXは、HTMLのようなコードをJavaScriptファイル内に記述できます
-
 
 // useState
 // state は useState という関数（React 内では Hooks といいます）を使うことで定義できる
 // 疑問：useStateはReactのフック？それともvue3のuseStateと同じ？
+
+// ② 
+// const [dogurl, setDogurl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
+// Reactでは、状態（state）を直接変更することはできない。setDogurlは状態を更新するための関数です。
+// 疑問：なぜ直接変更できないのか？
