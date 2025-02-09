1d349d94a1e995adc73f64d26158538d0ef984bb
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-09 16:12:14 +0900

    station3

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ export const App = () => {
   return (
     <div>
       <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="犬の画像" />
     </div>
   )
 }
