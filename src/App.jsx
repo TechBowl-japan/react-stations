855a1527794e1ede86a3ecf3d6410b4c130fcaed
kb8864 <yt7s858864@gmail.com>
2025-09-17 14:38:20 +0900

    react環境構築と簡単なヘッダーを追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,18 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const appName = "Dogアプリ";
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header className="app-header">
+        <div className="container">
+          <h1 className="brand">{appName}</h1>
+        </div>
+    </header>
+    <main className = "container">
+      <p>Dogアプリ</p>
+    </main>
+    </>
   )
 }
