62d4566bc88aa2b2a57704cd2e0db6348301449d
tom106gunma <tom106.net@gmail.com>
2025-11-30 06:14:34 +0900

    useStateを使ってみよう

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/retriever-flatcoated/n02099267_2259.jpg',
+  )
   return (
     <div>
       <header>Dogアプリ</header>
-      <img
-        src="https://images.dog.ceo/breeds/retriever-flatcoated/n02099267_2259.jpg"
-        alt="dog"
-      />
+      <img src={dogUrl} alt="dog" />
     </div>
   )
 }
