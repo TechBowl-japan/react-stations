9db22d2ae0d5f0c2bdde2fc7112d8c87926e24a7
tom106gunma <tom106.net@gmail.com>
2025-11-30 05:39:42 +0900

    画像を表示

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ export const App = () => {
   return (
     <div>
       <header>Dogアプリ</header>
+      <img
+        src="https://images.dog.ceo/breeds/retriever-flatcoated/n02099267_2259.jpg"
+        alt="dog"
+      />
     </div>
   )
 }
